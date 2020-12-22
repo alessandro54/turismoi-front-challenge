5fa3db6e91738438dbeca616f433e41c46e1c2f8
Alessandro <par_214@hotmail.com>
2020-12-21 20:24:39 -0500

    Setting up the project with tailwindCSS

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Container from "./Container.svelte";
+</script>
+
+<main>
+	<Container/>
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
