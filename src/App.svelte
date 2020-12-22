f3dbb932066bc30562c68a56fc1b7b63f3581891
Alessandro <par_214@hotmail.com>
2020-12-22 15:49:14 -0500

    Refacoring and Component rearrangement

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,17 @@
 <script>
-	import Container from "./components/Tours.svelte";
+	import Tours from "./components/Tours.svelte";
+	import {fetchTours} from "../scripts/API";
+	const promise = fetchTours();
 </script>
 
 <main>
-	<Container/>
+	{#await promise}
+		<p>...loading</p>
+	{:then {data,included}}
+		<Tours data={data} included={included}/>
+	{:catch error}
+		<p>There was an error!</p>
+	{/await}
 </main>
 
 <style global lang="postcss">
--- src/components/Tours.svelte
+++ src/components/Tours.svelte
@@ -1,8 +1,6 @@
 <script>
+  export let data,included
   import Tour from "./Tour.svelte";
-  import {fetchTours} from "../../scripts/API";
-  const promise = fetchTours();
-
   const getActivities = (relationships, included) => {
     return relationships.map((relation) => (
       included.find(activity => activity.id === relation.id).attributes))
@@ -10,16 +8,10 @@
 </script>
 
 <section class="grid grid-cols-1 md:grid-cols-2 md:gap-x-10 lg:grid-cols-3 xl:grid-cols-2 p-2 lg:pr-20 lg:pl-20 2xl:pr-60 2xl:pl-60">
-  {#await promise}
-    <p>...loading</p>
-  {:then {data,included}}
-    {#each data as tour}
-      <Tour
-        attributes={tour.attributes}
-        activities = {getActivities(tour.relationships.activities.data,included)}
-      />
-    {/each}
-  {:catch error}
-    <p>There was an error!</p>
-  {/await}
+  {#each data as tour}
+    <Tour
+      attributes={tour.attributes}
+      activities = {getActivities(tour.relationships.activities.data,included)}
+    />
+  {/each}
 </section>
